91866da233d4f194492e3825ae8c4820a694abb1
Gabriel <gabrielsmenezes99@gmail.com>
2022-01-05 09:56:10 -0300

    component refresh

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const DEFAULT_TITLE = 'Menezes Gaming guides';
+	let title = DEFAULT_TITLE;
+
+	const resetTitle = () => {
+		title = DEFAULT_TITLE;
+	};
+</script>
+
+<div class="index">
+	<h2>{title}</h2>
+	<p>
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum beatae perferendis fugiat dolor
+		deserunt in ad sit ipsa possimus exercitationem?
+	</p>
+
+	<button on:click={resetTitle}>Reset Title </button>
+	<input type="text" bind:value={title} name="title" />
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
