4a7d1c237371ffedf9ecc18a5c10b3e7ba2c1b89
Gabriel <gabrielsmenezes99@gmail.com>
2022-01-11 16:02:07 -0300

    Loading data & SSR

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -1,7 +1,35 @@
+<!-- Server Side -->
+<script context="module">
+	// Svelte Kit run this function in server on load component
+	export async function load({ fetch }) {
+		const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+		const guides = await res.json();
+
+		if (res.ok) {
+			return {
+				props: {
+					guides
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error('Could not fetch the guides')
+		};
+	}
+</script>
+
+<!-- Client Side -->
+<script>
+	export let guides;
+</script>
+
 <div class="guides">
 	<ul>
-		<li><a href="/">guide 1</a></li>
-		<li><a href="/">guide 2</a></li>
+		{#each guides as guide}
+			<li><a href="/">{guide.title}</a></li>
+		{/each}
 	</ul>
 </div>
 
