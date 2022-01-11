4c9e00b62fb14061eb29533d737b5739e647c8ae
Gabriel <gabrielsmenezes99@gmail.com>
2022-01-11 15:53:43 -0300

    reset layouts

---
+++ src/routes/guides/__layout.reset.svelte
@@ -0,0 +1,57 @@
+<script>
+	import '../../styles/global.css';
+
+	import { APP_NAME } from '../../utils/constants';
+	import Title from '$lib/title.svelte';
+
+	export let pageTitle = '';
+
+	let footerTitle = pageTitle ? `${APP_NAME} ${pageTitle}` : APP_NAME;
+</script>
+
+<header>
+	<nav>
+		<Title page={pageTitle} />
+		<div class="links">
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+			<a href="/guides">Guides</a>
+		</div>
+	</nav>
+</header>
+
+<main>
+	<!-- Page Content -->
+	<!-- Slot Tag mark in html template where the children will rendered -->
+	<slot />
+</main>
+
+<footer>
+	<p>Copyright 2022 {footerTitle}</p>
+</footer>
+
+<style>
+	header {
+		text-align: center;
+		background: rgba(0, 0, 0, 0.1);
+	}
+	main {
+		max-width: 960px;
+		margin: 20px auto;
+	}
+	footer {
+		text-align: center;
+	}
+	.links {
+		margin-left: auto;
+	}
+	a {
+		margin-left: 10px;
+	}
+	nav {
+		display: flex;
+		align-items: center;
+		max-width: 960px;
+		margin: 0 auto;
+	}
+</style>
